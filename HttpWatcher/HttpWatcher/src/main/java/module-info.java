84c6cca0244bf887248f5f608dc1e2c74079b5fd
Kovács Martin <martinkovacs22@gmail.com>
2024-05-28 17:27:06 +0200

    FXML DOC upload

---
+++ HttpWatcher/HttpWatcher/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.mycompany.httpwatcher {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.httpwatcher to javafx.fxml;
+    exports com.mycompany.httpwatcher;
+}
+*/
