8b926f84b64f421997a53c3d147bf9ef5b359227
Kovács Martin <martinkovacs22@gmail.com>
2024-05-29 11:04:31 +0200

    ProjectLost

--- HttpWatcher/HttpWatcher/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/* doesn't work with source level 1.8:
-module com.mycompany.httpwatcher {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.httpwatcher to javafx.fxml;
-    exports com.mycompany.httpwatcher;
-}
-*/
